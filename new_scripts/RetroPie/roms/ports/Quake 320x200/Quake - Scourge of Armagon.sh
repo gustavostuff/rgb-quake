106b7e15c23ec338a34e44fa2fd9dba1810736ea
Gustavo Lara <ing.glaragomez@gmail.com>
2021-03-01 15:19:14 -0700

    Delete new_scripts directory

--- new_scripts/RetroPie/roms/ports/Quake 320x200/Quake - Scourge of Armagon.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pakFile="/home/pi/quake/mp1/pak0.pak"
-if test -f "$pakFile"; then
-  vcgencmd hdmi_timings 1920 1 48 192 240 240 1 9 5 9 0 0 0 60.0 0 37872000 1
-  fbset -depth 8 && fbset -depth 24 && fbset -depth 32
-  cp /home/pi/quake/200-lines-core-options.cfg /home/pi/quake/retroarch-core-options.cfg
-  /opt/retropie/emulators/retroarch/bin/retroarch -L /opt/retropie/libretrocores/lr-tyrquake/tyrquake_libretro.so --config /home/pi/quake/config-200-lines.cfg "$pakFile"
-fi
